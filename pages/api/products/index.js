9654cf3e811cbaced0ccccd5697ab193e4a29bd6
Alonso-Pablo <pablo_nicolasalonso@yahoo.com>
2022-02-20 14:11:01 -0300

    refactor: modified responses and improve name for functions

--- pages/api/products/index.js
+++ pages/api/products/index.js
@@ -1,22 +1,31 @@
-// import {Database} from '../../../database/db';
 const Database = require('../../../database/db');
 
-const db = new Database();
+const db = new Database()
 
-const Server = async (req, res) => {
+const getProducts = async (req, res) => {
   
   if (req.method === 'GET') {
       try {
-        const allData = await db.getAll();
-        res.status(200).send({ data: allData });
+        const allData = await db.getAll()
+        res.status(200).send({
+          "statusCode": 200,
+          data: allData 
+        });
       } catch (e) {
-        console.error(e);
-        res.status(500).send({ data, error: 'Something went wrong' });
+        console.error(e)
+        res.status(500).send({
+          "statusCode": 500,
+          "data": 'Something went wrong'
+        });
     }
   } else {
-    res.status(404).send({ data, error: 'Something went wrong...' });
+    res.status(409).json({
+      "statusCode": 405,
+      "data": 'Method Not Allowed'
+    });
   }
 
 }
 
-export default Server
+export default getProducts
+
